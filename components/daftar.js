98740467d8fd7a4f49023a7ff1b9cb06296f7465
MOH. BUSTOMI RAMDANA <91039244+Bustomi21@users.noreply.github.com>
2022-07-08 10:53:34 +0700

    update daftar.js

--- components/daftar.js
+++ components/daftar.js
@@ -2,12 +2,12 @@
 export default function Daftar () {
     return (
         <div className="wrapper">
-            <h4>This Is Our Interior List</h4>
+            <h4>This Is Our Camera Price List</h4>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been </p>
             <div className = "daftar">
                 <div className="design">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 1</h4>
+                    <img src = "https://www.yatekno.com/wp-content/uploads/2018/08/Sony-A7R-II-YaTekno-1.jpg"/>
+                    <h4>CAMERA SONY A7R</h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
                     <div className="harga">
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
@@ -16,8 +16,8 @@ export default function Daftar () {
             </div>
             <div className = "daftar">
                 <div className="design_2">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 2</h4>
+                    <img src = "https://gudangdigitalonline.com/wp-content/uploads/2017/01/sony-alpha-a6000-kit-16-50mm-hitam-kamera-mirrorless_full01.jpg"/>
+                    <h4>CAMERA SONY ALPHA A-6000</h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
                     <div className="harga">
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
@@ -26,8 +26,8 @@ export default function Daftar () {
             </div>
             <div className = "daftar">
                 <div className="design">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 3</h4>
+                    <img src = "https://www.yatekno.com/wp-content/uploads/2018/08/Sony-A7R-II-YaTekno-1.jpg"/>
+                    <h4>CAMERA SONY A7R</h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
                     <div className="harga">
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
@@ -36,48 +36,8 @@ export default function Daftar () {
             </div>
             <div className = "daftar">
                 <div className="design_2">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 4</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
-                    <div className="harga">
-                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
-                    </div>
-                </div>
-            </div>
-            <div className = "daftar">
-                <div className="design">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 4</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
-                    <div className="harga">
-                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
-                    </div>
-                </div>
-            </div>
-            <div className = "daftar">
-                <div className="design_2">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 5</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
-                    <div className="harga">
-                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
-                    </div>
-                </div>
-            </div>
-            <div className = "daftar">
-                <div className="design">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 6</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
-                    <div className="harga">
-                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
-                    </div>
-                </div>
-            </div>
-            <div className = "daftar">
-                <div className="design_2">
-                    <img src = "https://images.pexels.com/photos/7750090/pexels-photo-7750090.jpeg?cs=srgb&dl=pexels-max-vakhtbovych-7750090.jpg&fm=jpg"/>
-                    <h4>Design 7</h4>
+                    <img src = "https://gudangdigitalonline.com/wp-content/uploads/2017/01/sony-alpha-a6000-kit-16-50mm-hitam-kamera-mirrorless_full01.jpg"/>
+                    <h4>CAMERA SONY ALPHA A-6000</h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
                     <div className="harga">
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
