5bd76b364d42b5916048eabfeb6368830b858aca
MOH. BUSTOMI RAMDANA <91039244+Bustomi21@users.noreply.github.com>
2022-07-08 10:54:59 +0700

    Update produc.js

--- components/produc.js
+++ components/produc.js
@@ -3,53 +3,25 @@ export default function Produc () {
         <div className="wrapper">
             <div className="product-list">
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/leica_m11s_m11_rangefinder_camera_silver_1642063766_1682570.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA LEICA</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
                 </div>
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/leica_m10_r_digital_rangefinder_camera_1594889195_1571535.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA LEICA</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
                 </div>
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/leica_s3_medium_format_dslr_1538995828_1436753.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
-                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="product-list">
-                <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
-                    <div className="keter">
-                        <h4>Roof Lamp</h4>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
-                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
-                    </div>
-                </div>
-                <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
-                    <div className="keter">
-                        <h4>Roof Lamp</h4>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
-                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
-                    </div>
-                </div>
-                <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
-                    <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA LEICA</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
@@ -59,25 +31,25 @@ export default function Produc () {
 
             <div className="product-list">
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_ilce_7m4k_b_alpha_a7_iv_mirrorless_1634813170_1668836.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA SONY</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
                 </div>
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_zv_e10_mirrorless_camera_with_1627386359_1655301.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA SONY</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
                 </div>
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_ilce7c_s_sony_alpha_a7c_mirrorless_1600118443_1594029.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA SONY</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
@@ -87,25 +59,53 @@ export default function Produc () {
 
             <div className="product-list">
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_alpha_a7r_iva_mirrorless_1649920973_1648073.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA SONY</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
                 </div>
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_ilce7m2k_b_alpha_a7ii_mirrorless_digital_1416995453_1102009.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA SONY</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
                 </div>
                 <div className="product">
-                    <img className="foto" src="https://s4.bukalapak.com/img/466219328/w-1000/lampu_gantung_minimalis_Terlaris.png"/>
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_ilce_7m4k_b_alpha_a7_iv_mirrorless_1634813170_1668836.jpg"/>
                     <div className="keter">
-                        <h4>Roof Lamp</h4>
+                        <h4>CAMERA SONY</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="product-list">
+                <div className="product">
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/fujifilm_instax_mini_90_neo_1494421571_1081641.jpg"/>
+                    <div className="keter">
+                        <h4>FUJIFILM</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
+                    </div>
+                </div>
+                <div className="product">
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/fujifilm_16696875_fujifilm_instax_40_1617901588_1630507.jpg"/>
+                    <div className="keter">
+                        <h4>FUJIFILM</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
+                        <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
+                    </div>
+                </div>
+                <div className="product">
+                    <img className="foto" src="https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/fujifilm_16654774_instax_mini_11_instant_1582663926_1548527.jpg"/>
+                    <div className="keter">
+                        <h4>FUJIFILM</h4>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been</p>
                         <p><b className="before">$100.000</b>   =  <b>$70.000</b></p>
                     </div>
