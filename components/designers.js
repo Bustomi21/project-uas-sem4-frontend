518c931153afd7f74b0378cd403cf0564d461d55
MOH. BUSTOMI RAMDANA <91039244+Bustomi21@users.noreply.github.com>
2022-07-08 10:48:31 +0700

    Update designers.js

--- components/designers.js
+++ components/designers.js
@@ -1,70 +1,108 @@
 export default function Designers() {
     return (
         <div className="wrapper">
-            <h4>This Is Our Designer List</h4>
+            <h4>This Is Our Combination List</h4>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been </p>
             <div className = "daftar_designer">
                 <div className="designer">
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
-                    <h4>mark zuckerberg</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/1_594f1527-5600-48fe-ab68-824629542b82_700x700.jpg?v=1561356404"/>
+                    <h4>Meike 35mm F1.4 Large</h4>
+                    <p>
+                        Specification:
+                        Focal Length:35mm
+                        Maximum Aperture: f/1.4
+                        Minimun Aperture: f/16
+                        Filter Size:49mm
+                        APS-C Visual angle:diagonal 43° horizontal 37° vertical 25°
+                        Lens construction is 8 elements in 5 groups, multi-layer coatings 
+                        Minimun Focus Distance:0.4m 1.31ft
+
+                        Length:2.6in
+                        Packing size: 3.6*2.8*3.6in
+                        Package Weight:300g,Product Weight:190
+
+                        Suitable for Canon EFM-mount Cameras,such as: EOS M M2 M3 M5 M6 M10 M50 M100 etc</p>
                 </div>
             </div>
             <div className = "daftar_designer">
                 <div className="designer_2">
                     <div className="text">
-                        <h4>mark zuckerberg</h4>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                        <h4>Meike Prime 85mm T2.1 Cine Lens for Full Frame Cinema Camera Systems</h4>
+                        <p>Specifications
+                            Lens Mount:	 PL/EF/RF/E/L
+                            Min Focal Dist:	85cm
+                            Filter Thread:	ø82
+                            Outer Diameter:	85mm
+                            Image Circle:	45mm
+                            Structure:	14 Elements in 8 Groups    
+                            Focal Length:	85mm
+                            Aperture Range:	T2.1-T22
+                            Iris Blades:	11    
+                            Gear Mod:	M0.8    
+                            Focus Rotation:	330 Degree </p>
                     </div>
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/1_d7b9390d-3a21-424f-a8be-77108612d32c_455x455.jpg?v=1594870469"/>
                 </div>
             </div>
             <div className = "daftar_designer">
                 <div className="designer">
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
-                    <h4>mark zuckerberg</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/5_aaf8b28f-36ef-48d1-ad7c-aa0c4784aa57_455x455.jpg?v=1617850711"/>
+                    <h4>MEKE 50mm F1.2 for Canon EOS EF Mount</h4>
+                        <p>Specification:
+                        Focal Length:50mm
+                        Maximum Aperture: f/1.2
+                        Minimun Aperture: f/22
+                        Filter Size:67mm
+
+                        APS-C Visual angle:diagonal 33.1° horizontal 25.8° vertical 17.4°
+
+                        Full frame Visual angle:diagonal 46.9° horizontal 39.3° vertical 26.4°
+
+                        Lens construction is 12 elements in 7 groups, multi-layer coatings 
+                        Minimun Focus Distance:0.6m
+                        Length:72mm
+                        Product Weight:620g </p>
                 </div>
             </div>
             <div className = "daftar_designer">
                 <div className="designer_2">
                     <div className="text">
-                        <h4>mark zuckerberg</h4>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                        <h4>Meike 35mm F1.4 Large</h4>
+                        <p>Specification: Focal Length:35mm Maximum Aperture: f/1.4 Minimun Aperture: f/16 Filter Size:49mm APS-C Visual angle:diagonal 43° horizontal 37° vertical 25° Lens construction is 8 elements in 5 groups, multi-layer coatings Minimun Focus Distance:0.4m 1.31ft Length:2.6in Packing size: 3.6*2.8*3.6in Package Weight:300g,Product Weight:190 Suitable for Canon EFM-mount Cameras,such as: EOS M M2 M3 M5 M6 M10 M50 M100 etc</p>
                     </div>
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/1_594f1527-5600-48fe-ab68-824629542b82_700x700.jpg?v=1561356404"/>
                 </div>
             </div>
             <div className = "daftar_designer">
                 <div className="designer">
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
-                    <h4>mark zuckerberg</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/1_d7b9390d-3a21-424f-a8be-77108612d32c_455x455.jpg?v=1594870469"/>
+                    <h4>Meike Prime 85mm T2.1</h4>
+                    <p>Specifications Lens Mount: PL/EF/RF/E/L Min Focal Dist: 85cm Filter Thread: ø82 Outer Diameter: 85mm Image Circle: 45mm Structure: 14 Elements in 8 Groups Focal Length: 85mm Aperture Range: T2.1-T22 Iris Blades: 11 Gear Mod: M0.8 Focus Rotation: 330 Degree</p>
                 </div>
             </div>
             <div className = "daftar_designer">
                 <div className="designer_2">
                     <div className="text">
-                        <h4>mark zuckerberg</h4>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                        <h4>MEKE 50mm F1.2 for Canon EOS EF Mount</h4>
+                        <p>Specification: Focal Length:50mm Maximum Aperture: f/1.2 Minimun Aperture: f/22 Filter Size:67mm APS-C Visual angle:diagonal 33.1° horizontal 25.8° vertical 17.4° Full frame Visual angle:diagonal 46.9° horizontal 39.3° vertical 26.4° Lens construction is 12 elements in 7 groups, multi-layer coatings Minimun Focus Distance:0.6m Length:72mm Product Weight:620g</p>
                     </div>
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/5_aaf8b28f-36ef-48d1-ad7c-aa0c4784aa57_455x455.jpg?v=1617850711"/>
                 </div>
             </div>
             <div className = "daftar_designer">
                 <div className="designer">
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
-                    <h4>mark zuckerberg</h4>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/1_594f1527-5600-48fe-ab68-824629542b82_700x700.jpg?v=1561356404"/>
+                    <h4>MEKE 50mm F1.2 for Canon EOS EF Mount</h4>
+                    <p>Specification: Focal Length:35mm Maximum Aperture: f/1.4 Minimun Aperture: f/16 Filter Size:49mm APS-C Visual angle:diagonal 43° horizontal 37° vertical 25° Lens construction is 8 elements in 5 groups, multi-layer coatings Minimun Focus Distance:0.4m 1.31ft Length:2.6in Packing size: 3.6*2.8*3.6in Package Weight:300g,Product Weight:190 Suitable for Canon EFM-mount Cameras,such as: EOS M M2 M3 M5 M6 M10 M50 M100 etc</p>
                 </div>
             </div>
             <div className = "daftar_designer">
                 <div className="designer_2">
                     <div className="text">
-                        <h4>mark zuckerberg</h4>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been  </p>
+                        <h4>Meike 35mm F1.4 Large</h4>
+                        <p>Specification: Focal Length:35mm Maximum Aperture: f/1.4 Minimun Aperture: f/16 Filter Size:49mm APS-C Visual angle:diagonal 43° horizontal 37° vertical 25° Lens construction is 8 elements in 5 groups, multi-layer coatings Minimun Focus Distance:0.4m 1.31ft Length:2.6in Packing size: 3.6*2.8*3.6in Package Weight:300g,Product Weight:190 Suitable for Canon EFM-mount Cameras,such as: EOS M M2 M3 M5 M6 M10 M50 M100 etc</p>
                     </div>
-                    <img src = "https://cdn.pulse2.com/cdn/2020/05/Mark-Zuckerberg.jpg"/>
+                    <img src = "https://cdn.shopify.com/s/files/1/0251/3508/7676/products/5_aaf8b28f-36ef-48d1-ad7c-aa0c4784aa57_455x455.jpg?v=1617850711"/>
                 </div>
             </div>
             
